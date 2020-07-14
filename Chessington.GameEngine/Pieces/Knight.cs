d66f0a7114084087a58ba25b37848f8cd49201f2
Alexander Chamberlain <Alexander.Chamberlain@softwire.com>
2020-07-14 14:17:30 +0100

    Knight movement

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,26 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            var availableMovesList = new List<Square>();
+
+            var x = new List<int> {1, 2, 2, 1, -1, -2, -2, -1};
+            var y = new List<int> {2, 1, -1, -2, -2, -1, 1, 2};
+
+            for (var i = 0; i < 8; i++)
+            {
+                var tryRow = currentSquare.Row + x[i];
+                var tryCol = currentSquare.Col + y[i];
+                if (InBounds(tryRow,tryCol))
+                {
+                    availableMovesList.Add(new Square(tryRow,tryCol));
+                }
+            }
+            
+            availableMovesList.RemoveAll(s => s == Square.At(currentSquare.Row, currentSquare.Col));
+            IEnumerable<Square> availableMoves = availableMovesList;
+            
+            return availableMoves;
         }
     }
 }
